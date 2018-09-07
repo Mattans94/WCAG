0be9f010b970ef121c5feefc9f189cf666253271
Emil <emil_emil94@hotmail.com>
2018-09-07 14:07:01 +0200

    some bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
 app.listen(port, () => {
   console.log('listening on port 3000');
 });
