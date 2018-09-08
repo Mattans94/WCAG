268ac70ce8cdc2bdadefd339c624efd7b3763e73
Marcus Thelin <marcusthelin97@gmail.com>
2018-09-08 14:19:28 +0200

    Removed unused code and added better comments

--- xml2json.js
+++ xml2json.js
@@ -15,21 +15,32 @@ mongoose
       fs.readFile(__dirname + '/data.xml', (err, data) => {
         parseString(data, (err, result) => {
           if (err) console.log(err);
+
+          //Make a constant called livsmedel that equals to the Livsmedel object in the data
           const {
             Livsmedel: livsmedel
           } = result.LivsmedelDataset.LivsmedelsLista[0];
-          console.log(livsmedel.length);
+          console.log('Found', livsmedel.length, 'objects');
 
+          /**
+           * Create a promise to handle async stuff.
+           * Then we can call the exit method when the promise
+           * is resolved, a.k.a in the .then() method
+           */
           let livsmedelLoop = new Promise((resolve, reject) => {
             livsmedel.forEach((obj, index, array) => {
-              // Loop through all livsmedel and save to the DB
-              const { Nummer, Namn, ViktGram, Huvudgrupp, Naringsvarden } = obj;
+              /**Loop through all livsmedel and save to the DB
+               * A forEach loop that creates a new Livsmedel on every iteration
+               * and then saves it to the database.
+               * The three dots (...) is called spread operator. It basically
+               * copies all the properties from the obj variable and pastes it in the new object.
+               */
               new Livsmedel({
                 ...obj
               })
                 .save()
                 .then(() => {
-                  console.log('SAVED', index);
+                  console.log('Saved item', index + 1);
                   if (index === array.length - 1) resolve();
                 })
                 .catch(err => console.log(err));
@@ -44,9 +55,3 @@ mongoose
       });
     });
   });
-
-// fs.writeFile(__dirname + '/data.json', jsonData, 'utf8', err => {
-//   if (err) console.log(err);
-//   console.log('Saved');
-//   process.exit();
-// });
