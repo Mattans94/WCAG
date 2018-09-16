2f6f5c18dd4f7523e1f457c8f0b48178da494c36
Marcus Thelin <marcusthelin97@gmail.com>
2018-09-16 20:37:45 +0200

    Made the hamburger animate into a cross

---
+++ public/main.js
@@ -0,0 +1,8 @@
+// Event listeners for hamburge icon
+$(document).on('show.bs.collapse', () => {
+  $('.hamburger').addClass('clicked');
+});
+
+$(document).on('hide.bs.collapse', () => {
+  $('.hamburger').removeClass('clicked');
+});
